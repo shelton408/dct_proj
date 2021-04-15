a341ab6729525517bada5bb324bd3d80d4e8bedc
Shelton Cai <shelton408@gmail.com>
2021-04-15 13:22:42 -0700

    matmul, 1d, and formula done

--- Sequential/seq_dct.py
+++ Sequential/seq_dct.py
@@ -6,9 +6,9 @@ import math
 import time
 from scipy.fftpack import dct
 
-z = 1/math.sqrt(2)
+z_norm = 1/math.sqrt(2)
 
-def seq_dct(mat):
+def formula_dct(mat):
     begin = time.time()
 
     y,x = mat.shape #x=y if square
@@ -19,20 +19,93 @@ def seq_dct(mat):
             for n1 in range(0, x):
                 for n2 in range(0, y):
                     dct_mat[k1, k2] += mat[n1, n2]*np.cos((np.pi/x)*(n1 + .5)*k1)*np.cos((np.pi/y)*(n2 + .5)*k2)
-    dct_mat = dct_mat * normalizer
+    result = dct_mat * normalizer
+
+    for k in range(0,x):
+        result[0][k] *= z_norm
+    for k in range(0,y):
+        result[k][0] *= z_norm
+
+    end = time.time()
+    t = end-begin
+    return result, t
+
+def oned_dct(mat):
+    x = mat.shape[0]
+    dct_mat = np.zeros(mat.shape[0])
+    for k in range(0,x):
+        for n in range(0,x):
+            dct_mat[k] +=mat[n]*np.cos((np.pi/x)*(n + .5)*k)
+    for k in range(0,x):
+        dct_mat[k] = dct_mat[k]*math.sqrt(2/x)
+    dct_mat[0] = dct_mat[0]*z_norm
+    print(dct(mat, type=2, norm='ortho'))
+    print(dct_mat)
+
+def double_1d_dct(mat):
+    begin = time.time()
+    
+    y,x = mat.shape #x=y if square
+    dct_mat = np.zeros(mat.shape)
+    dct_mat2 = np.zeros(mat.shape)
+    normalizer = math.sqrt(2/x) * math.sqrt(2/y)
+    for y_ in range(0,y):
+        for k in range(0,x):
+            for n in range(0,x):
+                dct_mat[y_, k] +=mat[y_, n]*np.cos((np.pi/x)*(n + .5)*k)
+    for i in range(0,y):
+        for j in range(0,x):
+            dct_mat[i,j] = dct_mat[i,j] * math.sqrt(2/x)*l(j)
+
+    for x_ in range(0,x):
+        for k in range(0,y):
+            for n in range(0,y):
+                dct_mat2[k, x_] +=dct_mat[n, x_]*np.cos((np.pi/y)*(n + .5)*k)
+    for i in range(0,y):
+        for j in range(0,x):
+            dct_mat2[i,j] = dct_mat2[i,j] * math.sqrt(2/y)*l(i)
+
+    result = dct_mat2
+
+    end = time.time()
+    t = end - begin
+    return result, t
+
+def matmul_dct(mat):
+    begin = time.time()
+    y, x = mat.shape
+    T = np.zeros(mat.shape)
+    for j in range(x):
+        T[0, j] = 1/math.sqrt(x)
+    for i in range(1, y):
+        for j in range(x):
+            T[i,j] = math.sqrt(2/x)*np.cos((2*j+1)*i*np.pi/(2*x))
+    
+    #avoid np.matmul since it can be automatically parallelized
+    #transpose may be optimized using built in but the cost of transpose shouldnt affect the order of computation
+    #matmul in O(N^3)
+    dct_mat = matmul(matmul(T, mat), T.T)
 
     end = time.time()
     t = end-begin
     return dct_mat, t
 
-def matmul_dct(mat):
-    return
+def matmul(a, b):
+    C = np.zeros((a.shape[0], b.shape[1]))
+
+    for i in range(a.shape[0]):
+        for j in range(b.shape[1]):
+            for k in range(b.shape[0]):
+                C[i,j] += a[i,k] * b[k,j]
+    
+    return C
+    
 
 #lambda, not used in our case
 def l(x):
-    if(x!=0):
-        return 1
-    return z
+    if(x==0):
+        return z_norm
+    return 1
 
 #to check correctness and also time refactorization method
 def scipy_dct(mat):
--- test_run.py
+++ test_run.py
@@ -5,26 +5,46 @@ import numpy as np
 from Sequential.seq_dct import *
 import timeit
 import pandas as pd
+import matplotlib.pyplot as plt
 
 
 #list out different methods run in test_suite
 #to check correctness, can compare anything to scipy dct
-TEST_CASES = ['scipy (refac)', 'seq by formula']
+TEST_CASES = ['N', 
+#'formula', 
+'double 1d-dct', 
+'matmul dct']
 def test_suite(shape):
     times = []
+    times.append(shape[0])#append N to the times for vizualization/graphing purposes
+
     random_mat = np.random.randint(10, size = shape)
 
-    result, t = scipy_dct(random_mat)
-    times.append(t)
+    if 'formula' in TEST_CASES:
+        result, t = formula_dct(random_mat)
+        times.append(t)
 
-    result, t = seq_dct(random_mat)
-    times.append(t)
+    if 'double 1d-dct' in TEST_CASES:
+        result, t = double_1d_dct(random_mat)
+        times.append(t)
 
+    if 'matmul dct' in TEST_CASES:
+        result, t = matmul_dct(random_mat)
+        times.append(t)
+
+    # result, t = scipy_dct(random_mat)
+    # times.append(t)
     return times
 
-shapes = [[x,x] for x in range(8,56,16)]
-results = [TEST_CASES]
+shapes = [[x,x] for x in [8,16,32,64, 128, 256]]
+results = []
 for shape in shapes:
     results.append(test_suite(shape))
 table = pd.DataFrame(results)
+table.columns = TEST_CASES
 print(table)
+ax = table.plot(kind='line', x='N', y=TEST_CASES[1])
+for case in TEST_CASES[2:]:
+    table.plot(kind='line', x='N', y=case, ax=ax)
+plt.show()
+
